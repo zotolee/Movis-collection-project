6256b8b54068c317f104cba4d946ec14ac031954
zotolee <akinayo711@gmail.com>
2022-04-27 21:43:48 -0700

    saving to github

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 
 
-class Counter   extends Component { 
+class Counter  extends Component { 
   state = {
       movies: getMovies()
   };
 
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies});
+  };
 
     render() { 
-        return ( 
+      const { length: count} = this.state.movies; /* using object desturing*/
+        if (count === 0)
+            return <p>There are no movies in the database again.</p>;
+
+        return (
+          <React.Fragment>
+            <p>Showing {count} movies in the database.</p>
           <table className="table">
             <thead>
               <tr>
@@ -17,18 +27,25 @@ class Counter   extends Component {
                 <th>Genre</th>
                 <th>Stock</th>
                 <th>Rate</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
-              {this.state.movies.map}
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
+              {this.state.movies.map(movie => (
+                <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button onClick={() => this.handleDelete(movie)} className="btn btn-outline-danger">Delete</button>
+                </td> 
+              </tr>
+              ))}
             </tbody>
           </table>
+          </React.Fragment> 
+          
         );
       }
 }
