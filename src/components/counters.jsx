35dd5ad984ba7c8de48ba3d79855acb39d899926
zotolee <akinayo711@gmail.com>
2022-04-28 14:48:40 -0700

    saving to github

---
+++ src/components/counters.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Counters extends Component {
+    state = { 
+
+     }; 
+
+    render() { 
+        return ;
+    }
+}
+ 
+export default Counters ;
